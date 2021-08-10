a65bec5c3a98ede90438e614f12f4073e5468ad0
Narcis Mestres <mestres@gmail.com>
2021-08-10 16:52:56 +0200

    Lab exercise: adde unit, integration and funtional test to calculator microservices.

--- calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
+++ calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
@@ -1,10 +1,22 @@
 package com.redhat.training;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 import com.redhat.training.service.SolverService;
 
+import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mockito;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 public class MultiplierResourceTest {
     
@@ -15,5 +27,42 @@ public class MultiplierResourceTest {
     public void setup() {
         solverService = mock(SolverService.class);
         multiplierResource = new MultiplierResource(solverService);
+    }
+    
+    @Test
+    public void twoPositiveValuesTest(){
+        // Given 2 positive numbers
+        when(solverService.solve("2")).thenReturn(Float.valueOf(2));
+        when(solverService.solve("2")).thenReturn(Float.valueOf(2));
+        // When you multiply
+        Float result = multiplierResource.multiply("2","2");
+
+        // Then result is correct and positive
+        assertEquals(Float.valueOf(4),result);
+    }
+
+    @Test
+    public void onePositiveOneNegativeValuesTest(){
+        // Given 2 positive numbers
+        when(solverService.solve("2")).thenReturn(Float.valueOf(2));
+        when(solverService.solve("-2")).thenReturn(Float.valueOf(-2));
+        // When you multiply
+        Float result = multiplierResource.multiply("2","-2");
+
+        // Then result is correct and positive
+        assertEquals(Float.valueOf(-4),result);
+    }
+
+    @Test
+    public void invalidValuesTest() throws NumberFormatException {
+        WebApplicationException cause = new WebApplicationException("Unknown error", Response.Status.BAD_REQUEST);
+        Mockito.when(solverService.solve("a")).thenThrow( new ResteasyWebApplicationException(cause) );
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        // When
+        Executable multiplication = () -> multiplierResource.multiply("a", "3");
+
+        // Then
+        assertThrows( ResteasyWebApplicationException.class, multiplication );
     }
 }
--- calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
+++ calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
@@ -2,9 +2,49 @@ package com.redhat.training;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 @TestHTTPEndpoint(SolverResource.class)
 public class SolverResourceTest {
 
+    @Test
+    public void simpleSumTest() {
+        given()
+                .pathParam("lhs", "2")
+                .pathParam("rhs", "3")
+        .when()
+                .get("/{lhs}+{rhs}")
+        .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body(is("5.0"));
+    }
+
+    @Test
+    public void simpleMultiplicationTest() {
+        given()
+                .pathParam("lhs", "2")
+                .pathParam("rhs", "3")
+        .when()
+                .get("/{lhs}*{rhs}")
+        .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body(is("6.0"));
+    }
+
+    @Test
+    public void invalidValuesTest() {
+        given()
+                .pathParam("lhs", "a")
+                .pathParam("rhs", "3")
+        .when()
+                .get("/{lhs}*{rhs}")
+        .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
 }
